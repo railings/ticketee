7747e1117efc006ac557c2177c68a760324bedaf
David Bahia <david.bahia@blueyonder.co.uk>
2012-11-09 00:31:11 +0000

    Added title functionality for show page

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -18,25 +18,29 @@ Then /^I should see "([^"]*)"$/ do |text|
   page.should have_content(text)
 end
 
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
 
 def path_to(page_name)
   case page_name
-
-  when /the home\s?page/
-    '/'
-
-  #when /the project page for "([^\"]*)"/
-  #  project_path(Project.find_by_name!($1))
-  # Add more mappings here.
-
-  else
-    begin
-      page_name =~ /the (.*) page/
-      path_components = $1.split(/\s+/)
-      self.send(path_components.push('path').join('_').to_sym)
-    rescue Object => e
-      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
-    end
+    when /the home\s?page/
+      '/'
+    when /the project page for "([^\"]*)"/
+      project_path(Project.find_by_name!($1))
+    else
+      begin
+        page_name =~ /the (.*) page/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue Object => e
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
   end
 end
