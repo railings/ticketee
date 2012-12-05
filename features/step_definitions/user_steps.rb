0a96bfc2ee2537e4e24ad181d6b630c3fe801890
David Bahia <david.bahia@blueyonder.co.uk>
2012-12-05 23:32:26 +0000

    Can dign in via Devise-provided form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,8 @@
 Given /^there are the following users:$/ do |table|
   # table is a | user@ticketee.com | password |pending
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.confirm! unless unconfirmed
   end
 end
