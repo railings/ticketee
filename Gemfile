e09a421d865d4bbafc81176ace6159b0aa37f233
David Bahia <david.bahia@blueyonder.co.uk>
2012-12-25 03:44:01 +0000

    Added ability to attach a file to a ticket, using paperclip gem

--- Gemfile
+++ Gemfile
@@ -27,3 +27,4 @@ end
 
 gem 'devise', '~> 1.4.3'
 gem 'cancan'
+gem 'paperclip'
--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -1,3 +1,7 @@
 Given /^I have run the seed task$/ do
   load Rails.root + "db/seeds.rb"
 end
+
+When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
+  attach_file(field, File.expand_path(path))
+end
