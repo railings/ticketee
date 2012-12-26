596af8c65c154d9723cbbff98c3214db36018c4c
David Bahia <david.bahia@blueyonder.co.uk>
2012-12-26 12:12:05 +0000

    Add files link using javascript - added chromedriver on path to use chrome instead of firefox

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,6 +12,12 @@ require 'cucumber/rails'
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
 
+# Use Chrome instead of Firefox
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.javascript_driver = :chrome
+
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how 
 # your application behaves in the production environment, where an error page will 
