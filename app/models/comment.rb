03f369f207311126bf6f4742734caa99895689c0
David Bahia <david.bahia@blueyonder.co.uk>
2013-01-06 22:30:32 +0000

    When updating a comment's status, it also updates the ticket's status

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,19 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :text, :user # , :ticket
+  attr_accessible :text, :user, :state_id, :ticket_id
+
+  after_create :set_ticket_state
+
+  belongs_to :ticket
+  delegate :project, :to => :ticket
+
+  belongs_to :user
+  belongs_to :state
 
   validates :text, :presence => true
 
-  #belongs_to :ticket
-  belongs_to :user
+  private
+  def set_ticket_state
+    self.ticket.state = self.state
+    self.ticket.save!
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  belongs_to :state
 
   attr_accessible :description, :title, :user, :assets_attributes
 
